3fdaa9bbba8a99cb1fe2b5620118939886229bb1
Vladislav Kovyazin <vvkovyazin@miem.hse.ru>
2021-08-14 18:02:56 +0300

    add goods list page

--- store/goods/views.py
+++ store/goods/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from .models import Good
+
+class GoodsView(TemplateView):
+    template_name = 'goods/goods.html'
+
+    def get(self, request):
+        all_ = Good.objects.all() # TODO: change to limit size
+        
+        return render(request, self.template_name, context={'goods': all_})
--- store/store/urls.py
+++ store/store/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.urls import path
 
 from wallets.views import WalletsRegisterView, WalletsLoginView, WalletsLoggingOutView, WalletsHomeView
+from goods.views import GoodsView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', GoodsView.as_view(), name='goods_list'),
     path('wallets', WalletsHomeView.as_view(), name='home'),
     path('wallets/register', WalletsRegisterView.as_view(), name='register'),
     path('wallets/login', WalletsLoginView.as_view(), name='login'),
     path('wallets/logout', WalletsLoggingOutView.as_view(), name='logout'),
+
 ]
