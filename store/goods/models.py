5d6fae2f3307426af11b194cebed63b368a0437b
Vladislav Kovyazin <vvkovyazin@miem.hse.ru>
2021-08-14 17:53:42 +0300

    add good model

--- store/goods/models.py
+++ store/goods/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+'''
+- Название
+- Цена
+- Описание
+- Категории
+- Картинки
+- Количество
+'''
+
+class Good(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    description = models.TextField(max_length=500, blank=True, null=False, default='No description!')
+    date_created = models.DateTimeField(auto_now_add=True)
