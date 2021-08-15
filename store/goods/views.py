3a4dc297f659fe2d4bead07f47393c704ba23523
Vladislav Kovyazin <vvkovyazin@miem.hse.ru>
2021-08-15 16:37:53 +0300

    show sellers of good

--- store/goods/views.py
+++ store/goods/views.py
@@ -1,16 +1,16 @@
+from django.db.models.query_utils import Q
 from django.http.response import Http404, HttpResponseBadRequest
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 from .models import Good
-
+from transactions.models import Transaction
 
 class GoodsView(TemplateView):
     template_name = 'goods/goods.html'
 
     def get(self, request):
         all_ = Good.objects.all()  # TODO: change to limit size
-
         return render(request, self.template_name, context={'goods': all_})
 
 
@@ -20,7 +20,9 @@ class GoodView(TemplateView):
     def get(self, request, id):
         try:
             good = Good.objects.get(id=id)
-            context = {'item': good}
-            return render(request, self.template_name, context=context)
         except Good.DoesNotExist:
             return HttpResponseBadRequest({})
+        selling = Transaction.objects.filter(seller__isnull=False, completed=False, buyer__isnull=True)
+        context = {'item': good, 'selling': selling}
+        return render(request, self.template_name, context=context)
+        
