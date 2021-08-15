c5de1a89f5ece24476f19f705a444743defd8726
Vladislav <vvkovyazin@miem.hse.ru>
2021-08-15 16:04:13 +0300

    refactor model

--- store/wallets/models.py
+++ store/wallets/models.py
@@ -1,11 +1,19 @@
-
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 
+
 from .managers import CustomUserManager
 
+
 class Wallet(AbstractBaseUser):
-    address = models.CharField(unique=True, primary_key=True, editable=False, max_length=64, null=False, blank=False)
+    address = models.CharField(unique=True,
+                               primary_key=True,
+                               editable=False,
+                               max_length=64,
+                               null=False,
+                               blank=False)
+
+
 
     USERNAME_FIELD = 'address'
     REQUIRED_FIELDS = []
