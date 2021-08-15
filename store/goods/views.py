b458757084806b8455ac4e385b96a2dcbb28ccff
Vladislav <vvkovyazin@miem.hse.ru>
2021-08-15 16:03:13 +0300

    add templates for goods

--- store/goods/views.py
+++ store/goods/views.py
@@ -1,12 +1,26 @@
+from django.http.response import Http404, HttpResponseBadRequest
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 from .models import Good
 
+
 class GoodsView(TemplateView):
     template_name = 'goods/goods.html'
 
     def get(self, request):
-        all_ = Good.objects.all() # TODO: change to limit size
-        
+        all_ = Good.objects.all()  # TODO: change to limit size
+
         return render(request, self.template_name, context={'goods': all_})
+
+
+class GoodView(TemplateView):
+    template_name = 'goods/good.html'
+
+    def get(self, request, id):
+        try:
+            good = Good.objects.get(id=id)
+            context = {'item': good}
+            return render(request, self.template_name, context=context)
+        except Good.DoesNotExist:
+            return HttpResponseBadRequest({})
