55820e539f5fa8497024e634a0f5e9f910f5ec4d
Vladislav <vvkovyazin@miem.hse.ru>
2021-08-15 16:03:53 +0300

    impl transactions

---
+++ store/transactions/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+from wallets.models import Wallet
+from goods.models import Good
+
+
+class Transaction(models.Model):
+    buyer = models.ForeignKey(Wallet,
+                              on_delete=models.SET_NULL,
+                              related_name='buyer',
+                              null=True)
+    seller = models.ForeignKey(Wallet,
+                               on_delete=models.SET_NULL,
+                               related_name='seller',
+                               null=True)
+    good = models.ForeignKey(Good,
+                             on_delete=models.PROTECT,
+                             null=True,
+                             related_name='selling_goods')
+    price = models.FloatField(null=False, default=0.0)
+
+    completed = models.BooleanField(default=False)
+# 2ed764583713451682774232822a111d
+    class SellerEqualBuyer(Exception):
+        pass
+
+    class SellerAndBuyerIsMissing(Exception):
+        pass
+
+    def save(self, *args, **kwargs):
+        if self.seller is None and self.buyer is None:
+            raise self.SellerAndBuyerIsMissing()
+        if self.seller == self.buyer:
+            raise self.SellerEqualBuyer("Seller cannot buy own goods.")
+        super().save(*args, **kwargs)
