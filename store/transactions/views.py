76d8d9198ba4b4c639674ec6d14e61fe005a232f
Vladislav Kovyazin <vvkovyazin@miem.hse.ru>
2021-08-15 17:37:06 +0300

    implement buying

--- store/transactions/views.py
+++ store/transactions/views.py
@@ -2,6 +2,7 @@ from django.http.response import HttpResponseBadRequest, HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from wallets.models import Wallet
 from wallets.services import get_balance
@@ -26,12 +27,21 @@ class FundInView(TemplateView):
         return HttpResponseRedirect(reverse('fundin'))
 
 
-class BuyView(TemplateView):
+class BuyView(LoginRequiredMixin, TemplateView):
+    login_url = 'wallets/login'
     template_name = 'transactions/buy.html'
 
     def get(self, request, id):
         trans = Transaction.objects.get(id=id)
         return render(request, self.template_name, context={'transaction': trans})
+    
+    def post(self, request, id):
+        trans: Transaction = Transaction.objects.get(id=id)
+        if not trans.completed:
+            trans.buyer = request.user
+            trans.completed = True
+            trans.save()
+        return HttpResponseRedirect(reverse('goods_detail', args=[trans.good.id]))
 
 class SellView(TemplateView):
     template_name = 'transactions/sell.html'
