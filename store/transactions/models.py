040fe17cf7caa12017f2171472d33053f56ced23
Vladislav <vvkovyazin@miem.hse.ru>
2021-08-16 23:17:50 +0300

    add buying and  integrity check

--- store/transactions/models.py
+++ store/transactions/models.py
@@ -20,16 +20,30 @@ class Transaction(models.Model):
     price = models.FloatField(null=False, default=0.0)
 
     completed = models.BooleanField(default=False)
-    
+
     class SellerEqualBuyer(Exception):
         pass
 
     class SellerAndBuyerIsMissing(Exception):
         pass
 
+    def complete(self):
+        self.completed = True
+        self.save(update_fields=['completed'])
+
+    def buy_by(self, user):
+        if not self.completed:
+            self.buyer = user
+            self.completed = True
+            self.save(update_fields=['completed', 'buyer'])
+
     def save(self, *args, **kwargs):
+        self.check_integrity()
+        super().save(*args, **kwargs)
+
+    def check_integrity(self):
         if self.seller is None and self.buyer is None:
-            raise self.SellerAndBuyerIsMissing()
+            raise self.SellerAndBuyerIsMissing(
+                'No seller and buyer in transaction.')
         if self.seller == self.buyer:
             raise self.SellerEqualBuyer("Seller cannot buy own goods.")
-        super().save(*args, **kwargs)
