6affaa714e8df8ba1b45deefc1b99a2475fe1bbd
Vladislav <vvkovyazin@miem.hse.ru>
2021-08-16 23:16:28 +0300

    formatting

--- store/goods/models.py
+++ store/goods/models.py
@@ -1,15 +1,10 @@
 from django.db import models
 
-'''
-- Название
-- Цена
-- Описание
-- Категории
-- Картинки
-- Количество
-'''
 
 class Good(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
-    description = models.TextField(max_length=500, blank=True, null=False, default='No description!')
+    description = models.TextField(max_length=500,
+                                   blank=True,
+                                   null=False,
+                                   default='No description!')
     date_created = models.DateTimeField(auto_now_add=True)
