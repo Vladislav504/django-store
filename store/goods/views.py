bf82e0962ef5ea386e872c464eba40705bb32d79
Vladislav <vvkovyazin@miem.hse.ru>
2021-08-16 19:46:17 +0300

    move good get to dispatch

--- store/goods/views.py
+++ store/goods/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 from django.http.response import HttpResponseBadRequest, HttpResponseRedirect
 from django.shortcuts import render
@@ -16,35 +17,34 @@ class GoodsView(TemplateView):
         return render(request, self.template_name, context={'goods': all_})
 
 
-class GoodView(TemplateView):
+class GoodView(LoginRequiredMixin, TemplateView):
     template_name = 'goods/good.html'
 
-    def get(self, request, id):
+    def dispatch(self, request, *args, **kwargs):
+        id = kwargs.get('id')
         try:
-            good = Good.objects.get(id=id)
+            self.good = Good.objects.get(id=id)
         except Good.DoesNotExist:
             return HttpResponseBadRequest({})
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, id):
         selling = Transaction.objects.filter(
             seller__isnull=False, completed=False,
             buyer__isnull=True).order_by('price')
         buying = Transaction.objects.filter(
             seller__isnull=True, completed=False,
             buyer__isnull=False).order_by('price')
-        context = {'item': good, 'selling': selling, 'buying': buying}
+        context = {'item': self.good, 'selling': selling, 'buying': buying}
         return render(request, self.template_name, context=context)
 
-    @login_required
     def post(self, request, id):
-        try:
-            good = Good.objects.get(id=id)
-        except Good.DoesNotExist:
-            return HttpResponseBadRequest({})
         try:
             price = float(request.POST['price'])
         except ValueError:
             return HttpResponseBadRequest("Inappropriate value")
         type_ = request.POST['type']
-        trans = Transaction(good=good, completed=False, price=price)
+        trans = Transaction(good=self.good, completed=False, price=price)
         if type_ == 'sell':
             trans.seller = request.user
         elif type_ == 'buy':
