c93508e58901cf8cb12403ad3397e92e5d4ff9ba
Vladislav <vvkovyazin@miem.hse.ru>
2021-08-21 10:33:50 +0300

    refactor manager

--- store/wallets/managers.py
+++ store/wallets/managers.py
@@ -1,7 +1,5 @@
-from django.contrib.auth.base_user import BaseUserManager
-from django.utils.translation import ugettext_lazy as _
-
 import uuid
+from django.contrib.auth.base_user import BaseUserManager
 
 
 class CustomUserManager(BaseUserManager):
